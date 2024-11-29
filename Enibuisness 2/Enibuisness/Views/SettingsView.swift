5f2ce27603bd1d515a75823bf665ca83b3bf918c
Bilal Mughal <bilal@cinqtech.com>
2024-11-29 18:27:30 +0500

    working fine unibuisness.

--- Enibuisness 2/Enibuisness/Views/SettingsView.swift
+++ Enibuisness 2/Enibuisness/Views/SettingsView.swift
@@ -154,12 +154,26 @@ struct MenuDouble: View {
 // Define the MenuButton view
 struct MenuButton: View {
     
+    @State var dateOfBirth: String = ""
+    @State var PhoneNumber: String = ""
+    @State var email: String = ""
+    @State var profession: String = "Buisness Developer"
+    @State var company: String = ""
+    @State var location: String = ""
+    @State var website: String = ""
+    @State var isWeeklyPresentation: Bool = true
+    
     var menuNature: menuType? = nil
+    
     
     var text: String
     
     var body: some View {
         Button {
+            
+            if menuNature == .presentation {
+                isWeeklyPresentation = true
+            }
             
         } label: {
             HStack {
@@ -171,37 +185,43 @@ struct MenuButton: View {
                 switch menuNature {
                     
                 case .dob:
-                    Text("Add")
+                    TextField("Add", text: $dateOfBirth)
                         .foregroundStyle(.gray.opacity(0.8))
                         .font(.headline)
+                        .multilineTextAlignment(.trailing)
                     
                 case .number:
-                    Text("+380 68 423 76")
+                    TextField("+380 68 423 76", text: $PhoneNumber)
                         .foregroundStyle(.gray.opacity(0.8))
                         .font(.headline)
+                        .multilineTextAlignment(.trailing)
                     
                 case .email:
-                    Text("adrienborg@outlook.com")
+                    TextField("email", text: $email)
                         .foregroundStyle(.gray.opacity(0.8))
                         .font(.headline)
-                        .allowsHitTesting(false)
-                        .accentColor(.gray.opacity(0.8))
-                        .underline()
+//                        .allowsHitTesting(false)
+//                        .accentColor(.gray.opacity(0.8))
+//                        .underline()
+                        .multilineTextAlignment(.trailing)
                     
                 case .profession:
-                    Text("Buisness developer")
+                    TextField("Buisness developer", text: $profession)
                         .foregroundStyle(.gray.opacity(0.8))
                         .font(.headline)
+                        .multilineTextAlignment(.trailing)
                     
                 case .compony:
-                    Text("ENUBUSINESS")
+                    TextField("ENUBUSINESS", text: $company)
                         .foregroundStyle(.gray.opacity(0.8))
                         .font(.headline)
+                        .multilineTextAlignment(.trailing)
                     
                 case .location:
-                    Text("Paris")
+                    TextField("Paris", text: $location)
                         .foregroundStyle(.gray.opacity(0.8))
                         .font(.headline)
+                        .multilineTextAlignment(.trailing)
                     
                 default:
                     EmptyView()
@@ -218,6 +238,40 @@ struct MenuButton: View {
         .padding(.vertical, 5)
         .background(Color(hex: "242424"))
         .cornerRadius(12)
+        .sheet(isPresented: $isWeeklyPresentation) {
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    // Section 1
+                  
+                    
+                    Text("1. A good referral would be someone who has inherited jewelry from a family member. \nThe jewelry may be sentimental, but not a style they would wear. We can create an updated treasured piece of jewelry using the original family gems. We will sort through the jewelry at no charge to see what costume is, and what is worth resetting. Working with the client will design an updated piece of jewelry still contains those family memories.\n\n2. Who do you know that has been dating for a long time? Have you heard them frustrated at all the misleading infor")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                    
+           
+                    
+                    Text("I have over 30 years' experience to guide them on this important quest.")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                    
+                    // Section 3
+                    Text("3. Have you noticed someone whose rings keep sliding and turning on their fingers or their fingers look like the ring is cutting off the blood supply of their finger?")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                    
+                    Text("They probably seem to tag or catch, or a stone has fallen out. Our onsite Goldsmith can take care of all their jewelry repair.")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 20).fill( Color(hex: "#242424")))
+                .padding(30)
+                .padding(.top)
+               
+            }.background(Color.black)
+            
+        }
     }
 }
 
